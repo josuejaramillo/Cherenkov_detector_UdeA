61279913d288bb97f6be834016ba6aaa827f5207
EAmilan <mamilanr@eafit.edu.co>
2025-09-08 19:05:55 -0500

    Updates

--- wcd/include/ParticleSource.hh
+++ wcd/include/ParticleSource.hh
@@ -1,4 +1,44 @@
+
 #ifndef PARTICLESOURCE_H
+#define PARTICLESOURCE_H
+
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+#include <vector>
+#include <string>
+#include <random>
+
+class ParticleSource : public G4VUserPrimaryGeneratorAction {
+public:
+    ParticleSource(int seed);
+    ~ParticleSource() override;
+    void GeneratePrimaries(G4Event* anEvent) override;
+
+private:
+    G4ParticleGun* particleGun;
+    std::vector<std::pair<double, double>> energyDistribution;
+    std::vector<std::pair<double, double>> phiDistribution;
+    std::vector<std::pair<double, double>> thetaDistribution;
+    std::default_random_engine gen;
+
+    struct ParticleEntry {
+        std::string name;
+        double weight;
+    };
+    std::vector<ParticleEntry> particleSpectrum;
+
+    void LoadDistribution(const std::string& filename, std::vector<std::pair<double, double>>& distribution);
+    double SampleFromDistribution(const std::vector<std::pair<double, double>>& distribution);
+    std::string SampleParticleType();
+};
+
+#endif // PARTICLESOURCE_H
+
+
+
+
+/*
+#endif
 #define PARTICLESOURCE_H
 
 #include "G4VUserPrimaryGeneratorAction.hh"
